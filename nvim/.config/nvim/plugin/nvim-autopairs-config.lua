ea0991e026f21086db74334cfc059ebf0178f3ba
jingshanyin <jingshan.yin@upstart.com>
2022-09-04 23:41:32 -0400

    more

--- nvim/.config/nvim/plugin/nvim-autopairs-config.lua
+++ nvim/.config/nvim/plugin/nvim-autopairs-config.lua
@@ -16,3 +16,9 @@ require("nvim-autopairs").setup ({
    map_c_h = false,  -- Map the <C-h> key to delete a pair
    map_c_w = false,-- map <c-w> to delete a pair if possible
 })
+
+require("nvim-treesitter.configs").setup {
+  endwise = {
+    enable = true,
+  },
+}
