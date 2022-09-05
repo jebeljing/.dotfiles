97188cb8579dc4ac63872c30038cbeec73b21596
jingshanyin <jingshan.yin@upstart.com>
2022-09-04 20:54:33 -0400

    vsnip

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -57,30 +57,61 @@ vim.api.nvim_set_keymap('', '<leader>hl', ":HopLine<CR>", opt)
 -- NOTE: You can use other key to expand snippet.
 
 -- Expand
-keymap("i", "<expr> <C-j>",  "vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'", {})
-keymap("s", "<expr> <C-j>",  "vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'", {})
-
---Expand or jump
-keymap("i", "<expr> <C-l>",   "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {})
-keymap("s", "<expr> <C-l>",   "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {})
-
---ump forward or backward
--- imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
--- smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
--- imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
--- smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-
--- Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
--- See https://github.com/hrsh7th/vim-vsnip/pull/50
--- nmap        s   <Plug>(vsnip-select-text)
--- xmap        s   <Plug>(vsnip-select-text)
--- nmap        S   <Plug>(vsnip-cut-text)
--- xmap        S   <Plug>(vsnip-cut-text)
+-- keymap("i", "<expr> <C-j> vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'", {})
+-- keymap("s", "<expr> <C-j>",  "vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'", {})
+--
+-- --Expand or jump
+-- keymap("i", "<expr> <C-l>",   "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {})
+-- keymap("s", "<expr> <C-l>",   "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {})
+--
+-- -- Jump forward or backward
+-- keymap("i", "<expr> <Tab>",   "vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {})
+-- keymap("s", "<expr> <Tab>",   "vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {})
+-- keymap("i", "<expr> <S-Tab>", "vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {})
+-- keymap("s", "<expr> <S-Tab>", "vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {})
+--
+-- -- Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+-- -- See https://github.com/hrsh7th/vim-vsnip/pull/50
+-- keymap("n", "s" ,  "<Plug>(vsnip-select-text)", {})
+-- keymap("x", "s" ,  "<Plug>(vsnip-select-text)", {})
+-- keymap("n", "S",   "<Plug>(vsnip-cut-text)", {})
+-- keymap("x", "S",   "<Plug>(vsnip-cut-text)", {})
 
 -- If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
 -- let g:vsnip_filetypes = {}
 -- let g:vsnip_filetypes.javascriptreact = ['javascript']
 -- let g:vsnip_filetypes.typescriptreact = ['typescript']
+--
+vim.api.nvim_command([[
+" NOTE: You can use other key to expand snippet.
+
+" Expand
+imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+
+" Expand or jump
+imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+
+" Jump forward or backward
+imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+" Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+" See https://github.com/hrsh7th/vim-vsnip/pull/50
+nmap        s   <Plug>(vsnip-select-text)
+xmap        s   <Plug>(vsnip-select-text)
+nmap        S   <Plug>(vsnip-cut-text)
+xmap        S   <Plug>(vsnip-cut-text)
+
+" If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
+let g:vsnip_filetypes = {}
+let g:vsnip_filetypes.javascriptreact = ['javascript']
+let g:vsnip_filetypes.typescriptreact = ['typescript']
+let g:vsnip_filetypes.ruby = ['ruby']
+]])
 
 
 
