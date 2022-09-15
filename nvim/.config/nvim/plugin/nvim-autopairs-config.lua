8c6c100c79529d10d66eb5411e5b416d62e14881
jingshanyin <jingshan.yin@upstart.com>
2022-09-15 00:53:21 -0400

    more

--- nvim/.config/nvim/plugin/nvim-autopairs-config.lua
+++ nvim/.config/nvim/plugin/nvim-autopairs-config.lua
@@ -5,11 +5,11 @@ require("nvim-autopairs").setup ({
    disable_filetype = { "TelescopePrompt" },
    disable_in_macro = false,  -- disable when recording or executing a macro,
    disable_in_visualblock = false, -- disable when insert after visual block mode
-  --  ignored_next_char = [=[[%w%%%'%[%"%.]]=]
-   ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+   ignored_next_char = [=[[%w%%%'%[%"%.]]=],
+   -- ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
    enable_moveright = true,
    enable_afterquote = true,  -- add bracket pairs after quote
-   enable_check_bracket_line = false,  --- check bracket in same line
+   enable_check_bracket_line = true,  --- check bracket in same line
    enable_bracket_in_quote = true, --
    enable_abbr = false, -- trigger abbreviation
    break_undo = true, -- switch for basic rule break undo sequence
