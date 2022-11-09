a22c1578d447cedd3f7e19e840817ab9a61e0184
jingshanyin <jingshan.yin@upstart.com>
2022-11-09 10:20:13 -0500

    set toggleterm float

--- nvim/.config/nvim/lua/jingshan/plugins/toggle-term.lua
+++ nvim/.config/nvim/lua/jingshan/plugins/toggle-term.lua
@@ -2,6 +2,5 @@ require("toggleterm").setup{
   open_mapping = [[<c-t>]],
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  direction = "float",
 }
-
-
